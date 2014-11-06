e386918138609af47178990ac6cc81d30d25b61f
nicolaslabbe <nicolas.labbe@adfab.fr>
2014-11-06 22:07:59 +0100

    #1 homepage

--- src/js/modules/reviens.js
+++ src/js/modules/reviens.js
@@ -7,6 +7,31 @@ var reviensApp = angular.module('reviensApp', [
                 controller: 'HomeController',
                 url: '/home',
                 templateUrl: 'partials/home.html'
+            })
+            .state('profil', {
+                controller: 'profilController',
+                url: '/profil',
+                templateUrl: 'partials/profil.html'
+            })
+            .state('objects', {
+                controller: 'objectsController',
+                url: '/objects',
+                templateUrl: 'partials/objects.html'
+            })
+            .state('friends', {
+                controller: 'friendsController',
+                url: '/friends',
+                templateUrl: 'partials/friends.html'
+            })
+            .state('notifications', {
+                controller: 'notificationsController',
+                url: '/notifications',
+                templateUrl: 'partials/notifications.html'
+            })
+            .state('search', {
+                controller: 'searchController',
+                url: '/search',
+                templateUrl: 'partials/search.html'
             });
 
         $urlRouterProvider.otherwise('/home');
